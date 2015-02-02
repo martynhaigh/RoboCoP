f4f70d59929861b57f60af1fbe594ffca51ecbcd
Martyn Haigh <martyn.haigh@gmail.com>
2015-02-02 22:22:40 +0000

    fix long incorrectly using a double type

--- src/main/java/com/rain/utils/android/robocop/model/StringUtils.java
+++ src/main/java/com/rain/utils/android/robocop/model/StringUtils.java
@@ -70,7 +70,9 @@ public class StringUtils {
             return "boolean";
         } else if (typeLower.equals(INT)) {
             return "int";
-        } else if (typeLower.equals(LONG) || typeLower.equals(DOUBLE) || typeLower.equals(DATE_TIME)) {
+        } else if (typeLower.equals(LONG)) {
+            return "long";
+        } else if (typeLower.equals(DOUBLE) || typeLower.equals(DATE_TIME)) {
             return "double";
         } else if (typeLower.equals(STRING)) {
             return "String";
